168147d8cbddb832b7863b3f4d9d14ef17fe65ce
NiccoCiuffi <nicko.ciuf@gmail.com>
2018-12-04 09:41:48 +0100

    visual update MASTER

--- EsercizioCompitoGIT/EsercizioCompitoGIT/Origine.cpp
+++ EsercizioCompitoGIT/EsercizioCompitoGIT/Origine.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void main() {
 	for (int iVar = 0; iVar < 10; iVar++) {
-		cout << "Numero: " << iVar + 1 << endl;
+		cout << "Numeri: " << iVar + 1 << endl;
 	}
 	system("PAUSE");
 }
